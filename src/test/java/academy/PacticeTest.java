8e158df26f2e22b72bae2bef82e534594607ab2a
swati <swati0605.singh@gmail.com>
2020-07-05 14:38:22 +0530

    this a asian timezone guy

---
+++ src/test/java/academy/PacticeTest.java
@@ -0,0 +1,15 @@
+package academy;
+
+import org.testng.annotations.Test;
+
+import resources1.BaseLib;
+
+public class PacticeTest extends BaseLib{
+	
+	@Test
+	public void practice()
+	{
+		System.out.println("its a practice test");
+	}
+
+}
