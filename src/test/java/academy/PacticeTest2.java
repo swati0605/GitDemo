f9cb2aca7644b9ef2dce887044d864fc457395d8
swati <swati0605.singh@gmail.com>
2020-07-05 17:16:55 +0530

    Asian developer

---
+++ src/test/java/academy/PacticeTest2.java
@@ -0,0 +1,26 @@
+package academy;
+
+import org.testng.annotations.Test;
+
+import resources1.BaseLib;
+
+public class PacticeTest2 extends BaseLib{
+	
+	@Test
+	public void practice444()
+	{
+		System.out.println("its a practice test444");
+	}
+	
+	
+	@Test
+	public void practice34()
+	{
+		System.out.println("its a practice test");
+		
+		System.out.println("its a practice test1");
+		
+		System.out.println("its a practice test3");
+	}
+
+}
